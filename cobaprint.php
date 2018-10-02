b1b6a1d78de58ae0dd57ea656cde7b1bdb4d0b44
jialingkun <djaka.tingkir2@gmail.com>
2018-10-02 18:12:33 +0700

    cleaning

--- cobaprint.php
+++ cobaprint.php
@@ -5,10 +5,12 @@ use Mike42\Escpos\Printer;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 try {
     // Enter the share name for your USB printer here
-    $connector = new WindowsPrintConnector("USBPRINTER"); //USBPRINTER adalah nama shared printernya
+    $connector = new WindowsPrintConnector("MINIPOS"); //MINIPOS adalah nama shared printernya
     /* Print a "Hello world" receipt" */
     $printer = new Printer($connector);
-    $printer -> text("Hello World!\n");
+    $printer -> text("Teh Matcha super nikmat terbuat dari daun asli\n");
+    $printer -> text("Rp 10.000\n");
+    $printer -> text("aaaaa\n");
     $printer -> cut();
     
     /* Close printer */
